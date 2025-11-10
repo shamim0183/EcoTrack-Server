3c87f68cb9fb5867a332fcd7b2913a03d282a348
shamim0183 <shaamim.hossaain@gmail.com>
2025-11-10 18:35:10 +0600

    Environment Setup Done

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
