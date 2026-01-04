cdc0fea0f8e6d6eab62c6be3e355f585c71af844
shamim0183 <shaamim.hossaain@gmail.com>
2026-01-04 15:07:51 +0600

    fix: Remove duplicate connectDB import

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 require("dotenv").config()
 const express = require("express")
 const cors = require("cors")
-const { connectDB } = require("./db")
 const { verifyFirebaseToken } = require("./firebaseAuth")
 
 const app = express()
